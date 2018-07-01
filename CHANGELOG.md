b937dedf2ff072833f3d49f513f6ca6610f33b7f
Wil Wilsman <wil@frontside.io>
2018-07-01 15:36:36 -0500

    Update build & deploy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 ### Added
 
 - `docs` script to generate documentation
+- transpiled es module bundle
 
 ## [0.3.3] - 2018-03-16
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,18 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
+  output: [{
     format: 'umd',
     name: 'BigTest.Mocha',
-    globals: { mocha: 'mocha' }
-  },
+    globals: { mocha: 'mocha' },
+    file: pkg.main
+  }, {
+    format: 'es',
+    file: pkg.module
+  }],
   external: ['mocha'],
   plugins: [
     resolve(),
